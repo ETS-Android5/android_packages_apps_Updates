dfe687db2dd60c7fd6cfb6d1ca30e05ea1ab6caf
yazidkucrit <yazid.kucrit@gmail.com>
2022-04-24 09:49:27 +0700

    Updates: Remove changelog button
    Co-Authored-By: Akito <akito@evolution-x.org>

--- src/org/evolution/ota/UpdatesActivity.java
+++ src/org/evolution/ota/UpdatesActivity.java
@@ -24,7 +24,6 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.ServiceConnection;
 import android.content.SharedPreferences;
-import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.IBinder;
